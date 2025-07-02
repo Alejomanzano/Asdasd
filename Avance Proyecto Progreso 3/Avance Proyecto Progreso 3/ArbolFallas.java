d10de5b575720f6bd029481c7b90883fa30076d6
Alejomanzano <152665581+Alejomanzano@users.noreply.github.com>
2025-07-02 16:21:23 -0500

    Add files via upload

---
+++ Avance Proyecto Progreso 3/Avance Proyecto Progreso 3/ArbolFallas.java
@@ -0,0 +1,37 @@
+public class ArbolFallas {
+    private NodoFalla raiz;
+
+    public void insertar(Fallas falla) {
+        raiz = insertarRec(raiz, falla);
+    }
+
+    private NodoFalla insertarRec(NodoFalla nodo, Fallas falla) {
+        if (nodo == null) {
+            return new NodoFalla(falla);
+        }
+
+        if (falla.getGravedad() < nodo.falla.getGravedad()) {
+            nodo.izquierda = insertarRec(nodo.izquierda, falla);
+        } else {
+            nodo.derecha = insertarRec(nodo.derecha, falla);
+        }
+
+        return nodo;
+    }
+
+    public String inOrden() {
+        StringBuilder sb = new StringBuilder();
+        inOrdenRec(raiz, sb);
+        return sb.toString();
+    }
+
+    private void inOrdenRec(NodoFalla nodo, StringBuilder sb) {
+        if (nodo != null) {
+            inOrdenRec(nodo.derecha, sb);
+
+            sb.append(nodo.falla.toString()).append("\n");
+            inOrdenRec(nodo.izquierda, sb);
+
+        }
+    }
+}
